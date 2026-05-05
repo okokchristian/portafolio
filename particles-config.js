eab7ceae0796c25827c9055906e4020ffc9d678f
Christian Quintana <itecs@MacBook-Pro-de-Christian.local>
2026-05-05 19:30:02 -0300

    first commit - portafolio

---
+++ particles-config.js
@@ -0,0 +1,43 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 50
+    },
+    color: {
+      value: "#9b8cc9"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5
+    },
+    size: {
+      value: 3
+    },
+    move: {
+      enable: true,
+      speed: 1
+    },
+    line_linked: {
+      enable: true,
+      distance: 120,
+      color: "#9b8cc9",
+      opacity: 0.3,
+      width: 1
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    },
+    modes: {
+      repulse: {
+        distance: 80
+      }
+    }
+  }
+});
